eef6ce0f2dc9d86c9fe8eb4833c141ec09479b8d
owenmcallister <owencmc@unc.edu>
2024-09-17 16:48:19 -0400

    Frontend that attempts to pull location

--- TourGuideFrontend/TourGuideFrontend/ContentView.swift
+++ TourGuideFrontend/TourGuideFrontend/ContentView.swift
@@ -1,24 +1,88 @@
-//
-//  ContentView.swift
-//  TourGuideFrontend
-//
-//  Created by Owen McAllister on 9/17/24.
-//
-
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
+    @State private var aiResponse: [String] = []
+    @State private var isLoading = false
+    @State private var errorMessage: String?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let location = locationManager.userLocation {
+                Text("Location: \(location.latitude), \(location.longitude)")
+                    .padding()
+
+                Button(action: {
+                    sendLocationToBackend(location: location)
+                }) {
+                    Text(isLoading ? "Loading..." : "Send Location to AI")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                if !aiResponse.isEmpty {
+                    List(aiResponse, id: \.self) { item in
+                        Text(item)
+                    }
+                }
+
+                if let error = errorMessage {
+                    Text("Error: \(error)")
+                        .foregroundColor(.red)
+                }
+
+            } else if locationManager.permissionDenied {
+                Text("Location Permission Denied.")
+                    .foregroundColor(.red)
+            } else {
+                Text("Requesting Location...")
+            }
         }
         .padding()
     }
-}
 
-#Preview {
-    ContentView()
+    func sendLocationToBackend(location: CLLocationCoordinate2D) {
+        isLoading = true
+        errorMessage = nil
+
+        let url = URL(string: "APIURL")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let body: [String: Any] = [
+            "latitude": location.latitude,
+            "longitude": location.longitude
+        ]
+        
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                isLoading = false
+            }
+            
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    errorMessage = error?.localizedDescription
+                }
+                return
+            }
+
+            if let responseList = try? JSONDecoder().decode([String].self, from: data) {
+                DispatchQueue.main.async {
+                    aiResponse = responseList
+                }
+            } else {
+                DispatchQueue.main.async {
+                    errorMessage = "Invalid response from server."
+                }
+            }
+        }
+
+        task.resume()
+    }
 }
---
+++ TourGuideFrontend/TourGuideFrontend/Models/LocationManager.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private var locationManager = CLLocationManager()
+
+    @Published var userLocation: CLLocationCoordinate2D?
+    @Published var permissionDenied = false
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            userLocation = location.coordinate
+            locationManager.stopUpdatingLocation()  // Stop updating to save battery
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while getting location: \(error.localizedDescription)")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .denied {
+            permissionDenied = true
+        }
+    }
+}
