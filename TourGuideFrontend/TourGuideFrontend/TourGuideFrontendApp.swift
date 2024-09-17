56937af11bbc9995a30e1a815e9e367c6c949b9e
owenmcallister <owencmc@unc.edu>
2024-09-17 13:59:54 -0400

    I love xcode!

---
+++ TourGuideFrontend/TourGuideFrontend/TourGuideFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourGuideFrontendApp.swift
+//  TourGuideFrontend
+//
+//  Created by Owen McAllister on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TourGuideFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
