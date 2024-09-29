8f739e25897c4ff38e2e7ff43a44dd92c65dfe64
AlexSieni <asieni@unc.edu>
2024-09-28 22:47:00 -0400

    Added map of location

---
+++ TourGuideFrontend/TourGuideFrontend/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  TourGuideFrontend
+//
+//  Created by Alex Sieni on 9/28/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
